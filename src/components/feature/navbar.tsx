def270d973956985d223cb6c06003e2d68dd47c1
Azukii09 <azuknisianta@gmail.com>
2024-06-13 09:50:27 +0700

    #7-initialize nav bar and create new branch
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

--- src/components/feature/navbar.tsx
+++ src/components/feature/navbar.tsx
@@ -1,40 +1,11 @@
 "use client"
-import Hamburger from "@/components/tokens/hamburger";
-import {useState} from "react";
-import Image from "next/image";
-import Link from "next/link";
-import Button from "@/components/tokens/button";
-import {signIn} from "next-auth/react";
 
 export default function Navbar(){
-    const [state, setState] = useState(false);
-    const [hamburger, setHamburger] = useState("");
-    const HandleHamburger = () => {
-        setHamburger("hamburger-active");
-        setState(!state);
-    }
-    const logo = "/assets/img/keaLogo.png";
+
 
     return(
-        <nav className="py-5 px-4 bg-slate-600 flex items-center justify-between">
-            <div className="flex items-center justify-between relative">
-                <Hamburger
-                    handler={HandleHamburger}
-                    action={state ? hamburger + " block absolute lg:hidden":"block absolute lg:hidden"}
-                />
-                <Link href={"/dashboard"} className="lg:flex hidden items-center justify-between">
-                    <Image
-                        src={logo}
-                        alt={"..."}
-                        width={100}
-                        height={100}
-                    />
-                </Link>
-
-            </div>
-            <div>
-                <Button typeName={"button"} className={"btn btn-info"} name={"Login"} handler={() => signIn("credentials")} />
-            </div>
+        <nav className="">
+            tes navbar
         </nav>
     )
 }
