63cbb087178eead5ffec701eb4f2140d25a55a85
Azukii09 <azuknisianta@gmail.com>
2024-06-15 08:15:20 +0700

    #13-finishing products display
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Slider from "@/components/feature/slider";
+import ProductSection from "@/components/feature/productSection";
 
 export default function HomePage() {
   return (
     <main>
-      <Slider/>
+        <Slider/>
+        <ProductSection/>
     </main>
   );
 }
---
+++ src/data/productList.ts
@@ -0,0 +1,70 @@
+const products = [
+    {
+        id:1,
+        fImg:"https://images.pexels.com/photos/7670761/pexels-photo-7670761.jpeg?auto=compress&cs=tinysrgb&w=800",
+        sImg:"https://images.pexels.com/photos/2688991/pexels-photo-2688991.jpeg?auto=compress&cs=tinysrgb&w=800",
+        productName:"Girls Powder",
+        price:49,
+        description:"Introducing our Girls' Powder, a touch of magic in every compact. Designed for youthful skin, it offers a silky-smooth finish with a hint of shimmer for a natural glow. Available in a range of shades to suit every girl's unique style, it's gentle and easy to apply, perfect for daily use. Elevate her beauty routine with our playful and practical powder, where beauty meets fun in one delightful package.",
+    },
+    {
+        id:2,
+        fImg:"https://images.pexels.com/photos/14716056/pexels-photo-14716056.jpeg?auto=compress&cs=tinysrgb&w=800",
+        sImg:"https://images.pexels.com/photos/14716151/pexels-photo-14716151.jpeg?auto=compress&cs=tinysrgb&w=800",
+        productName:"Red Lipstick",
+        price:86,
+        description:"Introducing our Lipstick Collection, where vibrant colors meet luxurious hydration. Each shade delivers intense pigmentation and a creamy texture that glides effortlessly onto lips, ensuring a flawless finish every time. Infused with nourishing ingredients like vitamin E and shea butter, our lipsticks keep lips smooth and moisturized. From bold reds to soft nudes, there's a shade for every occasion and mood. Elevate your look with our Lipstick Collection, where beauty and comfort collide in a single swipe.",
+    },
+    {
+        id:3,
+        fImg:"https://images.pexels.com/photos/8128065/pexels-photo-8128065.jpeg?auto=compress&cs=tinysrgb&w=800",
+        sImg:"https://images.pexels.com/photos/8128066/pexels-photo-8128066.jpeg?auto=compress&cs=tinysrgb&w=800",
+        productName:"Foundations",
+        price:102,
+        description:"Discover flawless beauty with our Foundation Makeup. Perfect for achieving a seamless complexion, our lightweight formula blends effortlessly, providing buildable coverage that lasts all day. Enriched with moisturizing agents and SPF protection, it not only conceals imperfections but also nourishes and shields your skin.Available in a diverse range of shades to match every skin tone, our Foundation Makeup ensures a radiant, natural-looking finish for all occasions. Redefine your beauty routine with our foundation—where confidence meets complexion perfection.",
+    },
+    {
+        id:4,
+        fImg:"https://images.pexels.com/photos/4889712/pexels-photo-4889712.jpeg?auto=compress&cs=tinysrgb&w=800",
+        sImg:"https://images.pexels.com/photos/4889720/pexels-photo-4889720.jpeg?auto=compress&cs=tinysrgb&w=800",
+        productName:"Eyeshadows",
+        price:57,
+        description:"Introducing our Eyeshadow Palette, your ultimate tool for captivating eyes. Dive into a spectrum of richly pigmented shades, from dazzling metallics to velvety mattes, designed to inspire endless creativity. Each buttery-smooth formula blends effortlessly, delivering intense color payoff that stays vibrant all day. Whether creating a subtle daytime look or a dramatic evening statement, our versatile palette ensures stunning results. Elevate your eye game with our Eyeshadow Palette—where innovation meets expression for unforgettable beauty.",
+    },
+    {
+        id:5,
+        fImg:"https://images.pexels.com/photos/7670761/pexels-photo-7670761.jpeg?auto=compress&cs=tinysrgb&w=800",
+        sImg:"https://images.pexels.com/photos/2688991/pexels-photo-2688991.jpeg?auto=compress&cs=tinysrgb&w=800",
+        productName:"Girls Powder",
+        price:49,
+        description:"Introducing our Girls' Powder, a touch of magic in every compact. Designed for youthful skin, it offers a silky-smooth finish with a hint of shimmer for a natural glow. Available in a range of shades to suit every girl's unique style, it's gentle and easy to apply, perfect for daily use. Elevate her beauty routine with our playful and practical powder, where beauty meets fun in one delightful package.",
+    },
+    {
+        id:6,
+        fImg:"https://images.pexels.com/photos/14716056/pexels-photo-14716056.jpeg?auto=compress&cs=tinysrgb&w=800",
+        sImg:"https://images.pexels.com/photos/14716151/pexels-photo-14716151.jpeg?auto=compress&cs=tinysrgb&w=800",
+        productName:"Red Lipstick",
+        price:86,
+        description:"Introducing our Lipstick Collection, where vibrant colors meet luxurious hydration. Each shade delivers intense pigmentation and a creamy texture that glides effortlessly onto lips, ensuring a flawless finish every time. Infused with nourishing ingredients like vitamin E and shea butter, our lipsticks keep lips smooth and moisturized. From bold reds to soft nudes, there's a shade for every occasion and mood. Elevate your look with our Lipstick Collection, where beauty and comfort collide in a single swipe.",
+    },
+    {
+        id:7,
+        fImg:"https://images.pexels.com/photos/8128065/pexels-photo-8128065.jpeg?auto=compress&cs=tinysrgb&w=800",
+        sImg:"https://images.pexels.com/photos/8128066/pexels-photo-8128066.jpeg?auto=compress&cs=tinysrgb&w=800",
+        productName:"Foundations",
+        price:102,
+        description:"Discover flawless beauty with our Foundation Makeup. Perfect for achieving a seamless complexion, our lightweight formula blends effortlessly, providing buildable coverage that lasts all day. Enriched with moisturizing agents and SPF protection, it not only conceals imperfections but also nourishes and shields your skin.Available in a diverse range of shades to match every skin tone, our Foundation Makeup ensures a radiant, natural-looking finish for all occasions. Redefine your beauty routine with our foundation—where confidence meets complexion perfection.",
+    },
+    {
+        id:8,
+        fImg:"https://images.pexels.com/photos/4889712/pexels-photo-4889712.jpeg?auto=compress&cs=tinysrgb&w=800",
+        sImg:"https://images.pexels.com/photos/4889720/pexels-photo-4889720.jpeg?auto=compress&cs=tinysrgb&w=800",
+        productName:"Eyeshadows",
+        price:57,
+        description:"Introducing our Eyeshadow Palette, your ultimate tool for captivating eyes. Dive into a spectrum of richly pigmented shades, from dazzling metallics to velvety mattes, designed to inspire endless creativity. Each buttery-smooth formula blends effortlessly, delivering intense color payoff that stays vibrant all day. Whether creating a subtle daytime look or a dramatic evening statement, our versatile palette ensures stunning results. Elevate your eye game with our Eyeshadow Palette—where innovation meets expression for unforgettable beauty.",
+    },
+]
+
+export const getProducts = () => {
+    return products;
+}
