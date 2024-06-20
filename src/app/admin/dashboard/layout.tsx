1c6dc5a04b7fcd1ea73475ab57d0d304f26a3e49
Azukii09 <azuknisianta@gmail.com>
2024-06-20 07:04:24 +0700

    #27- re-templating for route link
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

---
+++ src/app/admin/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function DashboardLayout({
+   children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <section>
+            tes
+            {children}
+        </section>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import Slider from "@/components/feature/end_user/content/slider";
 import ProductSection from "@/components/feature/end_user/content/productSection";
 import CategoriesSection from "@/components/feature/end_user/content/categoriesSection";
 import {getProducts} from "@/data/productList";
+import Navbar from "@/components/feature/end_user/template/navbar";
+import Footer from "@/components/feature/end_user/template/footer";
 
 export default function HomePage() {
   return (
     <main>
+        <Navbar/>
         <Slider/>
         <ProductSection title={"Featured Products"} data={getProducts()}/>
         <CategoriesSection/>
         <ProductSection title={"New Incoming Products"} data={getProducts()}/>
+        <Footer/>
     </main>
   );
 }
