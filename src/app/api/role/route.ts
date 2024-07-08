c037d5b5bbd36754b808c458c39d10ed96d13b88
Azukii09 <azuknisianta@gmail.com>
2024-07-08 13:07:56 +0700

    #4 - create new role functionality
    Signed-off-by: Azukii09 <azuknisianta@gmail.com>

---
+++ src/app/api/role/route.ts
@@ -0,0 +1,16 @@
+import {PrismaClient} from "@prisma/client";
+import {NextResponse} from "next/server";
+import type {Role} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const POST = async (req: Request) => {
+    const body:Role = await req.json();
+    const role = await prisma.role.create({
+        data:{
+            name: body.name,
+            desc: body.desc,
+        },
+    });
+    return NextResponse.json(role)
+}
